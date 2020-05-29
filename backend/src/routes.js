f311e44e97aeeca21bfd77892394b2cc3b41bb0d
Anderson Pimentel <apds.anderson@gmail.com>
2020-05-29 13:28:24 -0400

    chore: cria arquivo de conexao e rota de craicao de ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    return response.json({
-        evento: 'Criação do backend',
-        dev: 'Anderson Pimentel'
+routes.post('/ongs', async (request, response) => {
+    const {name, email, whatsapp, city, uf} = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
     });
+
+    return response.json({id});    
 });
 
 module.exports = routes;
