b8d4150cf724ac62aad0f57c4d74564fec421d02
Anderson Pimentel <apds.anderson@gmail.com>
2020-05-31 11:43:48 -0400

    chore: adiciona cors na app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
-
+const cors = require('cors')
 const app = express();
 
 // Converte body da req em json
 app.use(express.json());
+// Permite que os frontends possam acessar liberando o cross origin
+app.use(cors());
+// disponibiliza as rotas do app
 app.use(routes);
 
 
