6c0c958e99c42e1f7e8d8bdbabf7e172443830f6
Anderson Pimentel <apds.anderson@gmail.com>
2020-05-31 23:07:38 -0400

    chore(web): roteia de pagina de logon para pagina de registro

--- frontend/web/src/pages/Logon/index.js
+++ frontend/web/src/pages/Logon/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
-
+import {Link} from 'react-router-dom';
 
 import './styles.css';
 
@@ -17,10 +17,10 @@ export default function Logon() {
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID"/>
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#e02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
