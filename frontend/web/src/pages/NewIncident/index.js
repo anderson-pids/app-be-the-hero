42c5d0c1185644c205398bfe8d121c7f3debc992
Anderson Pimentel <apds.anderson@gmail.com>
2020-06-03 22:40:01 -0400

    feat(web): estilo da pagina de novos casos

--- frontend/web/src/pages/NewIncident/index.js
+++ frontend/web/src/pages/NewIncident/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import './style.css';
 
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
 export default function NewIncident() {
     return (
-        <h1>New Incident</h1>
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero"/>
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#e02041" />
+                        Voltar para o home
+                    </Link>
+                </section>
+
+                <form>
+                    <input placeholder="Título do caso"/>
+                    <textarea placeholder="Descrição"/>
+                    <input placeholder="Valor em reais"/>
+                    <button type="button">Cancelar</button>
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
     );
 }
