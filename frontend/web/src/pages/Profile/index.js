113784e06a07e048f9a3045e5dfec2ec98377a77
Anderson Pimentel <apds.anderson@gmail.com>
2020-06-05 10:29:52 -0400

    chore(web): Conecta pagina de casos ao backend

--- frontend/web/src/pages/Profile/index.js
+++ frontend/web/src/pages/Profile/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import './style.css';
 import logoImg from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
-
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
+import api from '../../services/api';
+
+import './style.css';
+
 export default function Profile() {
+    const [incidents, setIncidents] = useState([]);
+
+    const ongId = localStorage.getItem('ongId');
+    const ongName = localStorage.getItem('ongName');
+
+    // dispara função quando as variáveis do array mudam
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId
+            }
+        }).then(response => {
+            setIncidents(response.data)
+        });
+    }, [ongId]);
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" />
-                <span>Bem vinda, APAE</span>
+                <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
                 <button type="button">
                     <FiPower size={18} color="#E02041" />
@@ -20,59 +38,21 @@ export default function Profile() {
 
             <h1>Casos Cadastrados</h1>
             <ul>
-                <li>
+                {incidents.map(incident => (
+                    <li key={incidents.id}>
                     <strong>CASO:</strong>
-                    <p>Caso Teste</p>
+                    <p>{incident.title}</p>
 
                     <strong>DESCRIÇÃO</strong>
-                    <p>A cadelinha Julie foi atropelada</p>
+                    <p>{incident.description}</p>
 
                     <strong>VALOR:</strong>
-                    <p>R$ 120,00 reais</p>
+                    <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
                     <button type="button">
                         <FiTrash2 size={20} color="#a8a8b3" />
                     </button>
                 </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso Teste</p>
-
-                    <strong>DESCRIÇÃO</strong>
-                    <p>A cadelinha Julie foi atropelada</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00 reais</p>
-                    <button type="button"><
-                        FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso Teste</p>
-
-                    <strong>DESCRIÇÃO</strong>
-                    <p>A cadelinha Julie foi atropelada</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00 reais</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso Teste</p>
-
-                    <strong>DESCRIÇÃO</strong>
-                    <p>A cadelinha Julie foi atropelada</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00 reais</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-
+                ))}                
             </ul>
         </div>
     );
