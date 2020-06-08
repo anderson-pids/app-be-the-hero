ad6ee293c62677e227a1c0fdad5e0de77351fd71
Anderson Pimentel <apds.anderson@gmail.com>
2020-06-08 08:49:53 -0400

    feat(web): conecta novo indidente com api

--- frontend/web/src/pages/Profile/index.js
+++ frontend/web/src/pages/Profile/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import logoImg from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -11,6 +11,7 @@ import './style.css';
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
 
+    const history = useHistory();
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
 
@@ -33,11 +34,16 @@ export default function Profile() {
             }});
 
             setIncidents(
-                incidents.filter(incident => incident.id != id)
+                incidents.filter(incident => incident.id !== id)
             );
         } catch (error) {
             alert('Erro ao deletar caso, tente novamente!');
         }
+    }
+
+    const handleLogout = () => {
+        localStorage.clear();
+        history.push('/');
     }
 
     return (
@@ -46,7 +52,7 @@ export default function Profile() {
                 <img src={logoImg} alt="Be The Hero" />
                 <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
