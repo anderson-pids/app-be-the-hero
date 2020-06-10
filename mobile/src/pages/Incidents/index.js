41cca6b062cbbbdf9429694ee043edf7cbeb259e
Anderson Pimentel <apds.anderson@gmail.com>
2020-06-09 21:28:27 -0400

    chore(mobile): cria lista de casos

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View,Image,Text} from 'react-native';
-
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
@@ -18,7 +18,32 @@ export default function Incidents() {
             <Text style={styles.title}>Bem vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
 
+            <FlatList
+                style={styles.incidentList}
+                data={[1, 2, 3]}
+                renderItem={() => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAE</Text>
+
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAE</Text>
+
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAE</Text>
+
+                        <TouchableOpacity
+                            style={styles.detailsButton}
+                            onPress={() => { }}
+                        >
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name='arrow-right' size={17} color='#e02041' />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+
         </View>
     );
-    
+
 }
