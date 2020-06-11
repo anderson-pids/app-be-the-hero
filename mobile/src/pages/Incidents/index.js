078ac8436419f4ff776925410a114d3858fa6d83
Anderson Pimentel <apds.anderson@gmail.com>
2020-06-11 08:36:12 -0400

    chore(mobile): add scrool infinito em casos

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,15 +12,31 @@ export default function Incidents() {
     const navigation = useNavigation();
     const [incidents, setIncidents] = useState([]);
     const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
     const navigateToDetail = (incident) => {
         navigation.navigate('Detail', {incident});
     }
 
     const loadIncidents = async () => {
-        const response = await api.get('incidents');
-        setIncidents(response.data);
+        if(loading)
+            return;
+        
+        if(total > 0 && incidents.length === total)
+            return;
+
+        setLoading(true);
+
+        const response = await api.get('incidents', 
+            {params : {page}}
+        );
+        // setIncidents(response.data);
+        // anexando dois vetores dentro de um vetor
+        setIncidents([...incidents,...response.data]);
         setTotal(response.headers['x-total-count']);
+        setLoading(false);
+        setPage(page+1);
     }
 
     useEffect(() => {
@@ -43,6 +59,8 @@ export default function Incidents() {
                 data={incidents}
                 keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={( {item: incident} ) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
