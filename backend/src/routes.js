a5704a796252a018c98b75f71614519da43347fd
Anderson Pimentel <apds.anderson@gmail.com>
2020-06-22 18:39:27 -0400

    chore: Valida rotas para Query, Params e Headers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -20,9 +20,17 @@ routes.post('/ongs', celebrate(
     }
 ),OngController.create);
 
-routes.get('/incidents', IncidentController.index);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}),IncidentController.index);
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}),IncidentController.delete);
 
 routes.get('/profile', celebrate({
     [Segments.HEADERS]: Joi.object({
