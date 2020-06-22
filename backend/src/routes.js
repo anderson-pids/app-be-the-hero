e4b8bd914ebe2b67f847fc95ef753775530150d6
Anderson Pimentel <apds.anderson@gmail.com>
2020-06-22 00:53:04 -0400

    feat: Iniciando validacao de rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const routes = express.Router();
 
 const OngController = require('./controllers/OngController');
@@ -7,7 +8,17 @@ const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate(
+    {
+        [Segments.BODY]: Joi.object().keys({
+            name: Joi.string().required().min(2),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required().min(10).max(11),
+            city: Joi.string().required(),
+            uf: Joi.string().required().length(2)
+        })
+    }
+),OngController.create);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
